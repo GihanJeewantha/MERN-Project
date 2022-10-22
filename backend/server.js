f3191c0d92f3ca6652787b6549c479408d771cc2
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-10-22 22:35:10 +0530

    Update server.js
    get specific id

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,15 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send("API is running..");
 
-})
+});
 
 app.get('/api/notes',(req,res) =>{
     res.json(notes)
+});
+
+app.get('/api/notes/:id',(req,res) =>{
+    const note = note.find((n)=>n._id===req.params.id);
+    console.log(req.params);
 })
 
 app.listen(5000,console.log("server started on PORT 5000"));
